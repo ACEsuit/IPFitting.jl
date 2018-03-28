56e9b62bfbfcc20abb9a08ac42f7925601c19061
Genevieve <dusson@ljll.math.upmc.fr>
2018-03-28 16:12:19 +0100

    remove useless parts of fitting.

--- src/fitting.jl
+++ src/fitting.jl
@@ -7,22 +7,6 @@
 using Plots
 using DataFrames
 
-F(t) = sqrt(t)
-rho(r) = exp(-r)
-
-function Energy_tot(x)
-    #total energy computation for a given configuration x
-
-    #Input: matrix containing the configurations in row
-    # Ntrain = size(x,1)
-    # Nvariables = size(x,2)
-
-    return F.(rho.(x[:,1])+rho.(x[:,2])) +F.(rho.(x[:,2])+rho.(x[:,3])) +F.(rho.(x[:,3])+rho.(x[:,1])) #total energy
-    # return   F.(rho.(x[:,1])+rho.(x[:,2])+rho.(x[:,3])) #site energy
-
-end
-
-
 function regression(Y,Phi,lambda=0.000001)
     #Inputs
     #Y: vector containing exact values for the samples
@@ -38,36 +22,57 @@ function regression(Y,Phi,lambda=0.000001)
 end
 
 
-N = 3; #number of particles
+# F(t) = sqrt(t)
+# rho(r) = exp(-r)
+#
+# function Energy_tot(x)
+#     #total energy computation for a given configuration x
+#
+#     #Input: matrix containing the configurations in row
+#     # Ntrain = size(x,1)
+#     # Nvariables = size(x,2)
+#
+#     return F.(rho.(x[:,1])+rho.(x[:,2])) +F.(rho.(x[:,2])+rho.(x[:,3])) +F.(rho.(x[:,3])+rho.(x[:,1])) #total energy
+#     # return   F.(rho.(x[:,1])+rho.(x[:,2])+rho.(x[:,3])) #site energy
+#
+# end
+
+# N = 3; #number of particles
+#
+#
+#
+# #Generate samples
+# Ntrain = 1000;
+# Ntest = 2000;
+# LeftRcuttrain = 0.5;
+# RightRcuttrain = 3;
+#
+# LeftRcuttest = 0.5;
+# RightRcuttest = 3;
+#
+#
+# r1train = (RightRcuttrain-LeftRcuttrain)*rand(Ntrain) + LeftRcuttrain; #sample variables
+# r2train = (RightRcuttrain-LeftRcuttrain)*rand(Ntrain) + LeftRcuttrain;
+# r3train = (RightRcuttrain-LeftRcuttrain)*rand(Ntrain) + LeftRcuttrain;
+#
+# r1test = (RightRcuttest-LeftRcuttest)*rand(Ntest) + LeftRcuttest; #test variables
+# r2test = (RightRcuttest-LeftRcuttest)*rand(Ntest) + LeftRcuttest;;
+# r3test = (RightRcuttest-LeftRcuttest)*rand(Ntest) + LeftRcuttest;;
+#
+# Config_train = [r1train r2train r3train]; #configuration matrix
+# Etrain = Energy_tot(Config_train) #energies for the samples
+#
+# Config_test = [r1test r2test r3test]; #configuration matrix
+# Etest_exact = Energy_tot(Config_test); #energies for the test
+#
+#
+# Deg = collect(1:10);
 
 
 
-#Generate samples
-Ntrain = 1000;
-Ntest = 2000;
-LeftRcuttrain = 0.5;
-RightRcuttrain = 3;
-
-LeftRcuttest = 0.5;
-RightRcuttest = 3;
 
 
-r1train = (RightRcuttrain-LeftRcuttrain)*rand(Ntrain) + LeftRcuttrain; #sample variables
-r2train = (RightRcuttrain-LeftRcuttrain)*rand(Ntrain) + LeftRcuttrain;
-r3train = (RightRcuttrain-LeftRcuttrain)*rand(Ntrain) + LeftRcuttrain;
 
-r1test = (RightRcuttest-LeftRcuttest)*rand(Ntest) + LeftRcuttest; #test variables
-r2test = (RightRcuttest-LeftRcuttest)*rand(Ntest) + LeftRcuttest;;
-r3test = (RightRcuttest-LeftRcuttest)*rand(Ntest) + LeftRcuttest;;
-
-Config_train = [r1train r2train r3train]; #configuration matrix
-Etrain = Energy_tot(Config_train) #energies for the samples
-
-Config_test = [r1test r2test r3test]; #configuration matrix
-Etest_exact = Energy_tot(Config_test); #energies for the test
-
-
-Deg = collect(1:10);
 
 
 a = 1; #parameter for the exponentials
@@ -132,38 +137,38 @@ for (ibf,bf) in enumerate(Basis_fct_list)
 end
 
 
-df1 = DataFrame( deg = Deg )
-df1[Symbol("Nb_basis_fcts")] = NB_BASIS_FCTS[:,1];
-for k = 1:length(Basis_fct_list)
-    df1[Symbol(Basis_fct_list[k][2])] = RMSE[1:end,k];
-end
-println("RMSE")
-
-display(df1)
-
-
-df2 = DataFrame( deg = Deg )
-df2[Symbol("Nb_basis_fcts")] = NB_BASIS_FCTS[:,1];
-for k = 1:length(Basis_fct_list)
-    df2[Symbol(Basis_fct_list[k][2])] = Linfty_err[1:end,k];
-end
-println("Linfty_error")
-
-display(df2)
+# df1 = DataFrame( deg = Deg )
+# df1[Symbol("Nb_basis_fcts")] = NB_BASIS_FCTS[:,1];
+# for k = 1:length(Basis_fct_list)
+#     df1[Symbol(Basis_fct_list[k][2])] = RMSE[1:end,k];
+# end
+# println("RMSE")
+#
+# display(df1)
 
 
-Q = plot(Deg,RMSE[1:end,1], yscale = :log10, ylabel = "Root Mean Square Error", xlabel = "Polynomial degree",title = "Mean error versus polynomial degree",label = Basis_fct_list[1][2])
-
-for k=2:length(Basis_fct_list)
-    plot!(Deg,RMSE[1:end,k], label = Basis_fct_list[k][2])
-end
-display(Q)
-
-P = plot(NB_BASIS_FCTS[1:end,1],RMSE[1:end,1], yscale = :log10, ylabel = "Root Mean Square Error", xlabel = "Number of basis functions",title = "Mean error versus nb of basis functions",label = Basis_fct_list[1][2])
-
-for k=2:length(Basis_fct_list)
-    plot!(NB_BASIS_FCTS[1:end,1],RMSE[1:end,k], label = Basis_fct_list[k][2])
-end
-display(P)
+# df2 = DataFrame( deg = Deg )
+# df2[Symbol("Nb_basis_fcts")] = NB_BASIS_FCTS[:,1];
+# for k = 1:length(Basis_fct_list)
+#     df2[Symbol(Basis_fct_list[k][2])] = Linfty_err[1:end,k];
+# end
+# println("Linfty_error")
+#
+# display(df2)
+#
+#
+# Q = plot(Deg,RMSE[1:end,1], yscale = :log10, ylabel = "Root Mean Square Error", xlabel = "Polynomial degree",title = "Mean error versus polynomial degree",label = Basis_fct_list[1][2])
+#
+# for k=2:length(Basis_fct_list)
+#     plot!(Deg,RMSE[1:end,k], label = Basis_fct_list[k][2])
+# end
+# display(Q)
+#
+# P = plot(NB_BASIS_FCTS[1:end,1],RMSE[1:end,1], yscale = :log10, ylabel = "Root Mean Square Error", xlabel = "Number of basis functions",title = "Mean error versus nb of basis functions",label = Basis_fct_list[1][2])
+#
+# for k=2:length(Basis_fct_list)
+#     plot!(NB_BASIS_FCTS[1:end,1],RMSE[1:end,k], label = Basis_fct_list[k][2])
+# end
+# display(P)
 
 # end
