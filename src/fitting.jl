000bc59cb48013c807deeefdd93382d7b71b11e9
Christoph Ortner <c.ortner@warwick.ac.uk>
2018-04-04 03:07:01 +0100

    fitting to SW data now works very well

--- src/fitting.jl
+++ src/fitting.jl
@@ -24,7 +24,7 @@ function assemble_system(basis, data; verbose=true)
       pm = Progress(length(data) * length(basis))
    end
    for (id, d) in enumerate(data)
-      at = d[1]::Atoms{Float64, Int}
+      at = d[1]
       lenat = max(lenat, length(at))
       F[id] = d[2]::Float64
       for (ib, b) in enumerate(basis)
@@ -38,15 +38,15 @@ function assemble_system(basis, data; verbose=true)
 end
 
 function regression(basis, data; verbose = true)
-   println("assemble system")
+   verbose && println("assemble system")
    A, F, lenat = assemble_system(basis, data; verbose = verbose)
    # compute coefficients
-   println("solve lsq")
+   verbose && println("solve lsq")
    Q, R = qr(A)
    c = R \ (Q' * F)
    # check error on training set
-   println("rms error on training set: ",
-           norm(A * c - F) / sqrt(length(data)) / sqrt(lenat) )
+   verbose && println("rms error on training set: ",
+                       norm(A * c - F) / sqrt(length(data)) / sqrt(lenat) )
    return c
 end
 
@@ -54,11 +54,4 @@ end
 function rms(c, basis, data; verbose = false)
    A, F, lenat = assemble_system(basis, data; verbose=verbose)
    return norm(A * c - F) / sqrt(length(data)) / sqrt(lenat)
-end
-
-
-
-
-function fit_nbody(basis, r0, rcut)
-
 end
