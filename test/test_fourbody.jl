f9d0a10256ca7669829f534c71dca7061ecb37c7
Christoph Ortner <c.ortner@warwick.ac.uk>
2018-04-10 05:22:34 +0100

    additional deg-2 and deg-3 terms

--- test/test_fourbody.jl
+++ test/test_fourbody.jl
@@ -2,10 +2,21 @@
 using NBodyIPs
 using JuLIP, Base.Test, StaticArrays, BenchmarkTools, Combinatorics
 
-D = dict(:poly, 6)
-
-
+D = dict(:poly, 8)
 B = NBodyIPs.polys_fourbody(D...)
+Bsym = NBodyIPs.psym_polys_nbody(4, D...)
+
+# D = dict(:poly, 10)
+# B = NBodyIPs.polys_fourbody(D...)
+# Bsym = NBodyIPs.psym_polys_nbody(4, D...)
+
 
 display(B[4])
 ex, f, df = NBodyIPs.gen_fun(B[4], D...)
+
+
+
+D = dict(:poly, 4)
+
+
+B = NBodyIPs.polys_fourbody(6)
