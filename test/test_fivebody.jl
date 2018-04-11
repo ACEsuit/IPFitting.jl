7dadf146f34a7e00c7ac93187424f7324b993373
Genevieve <dusson@ljll.math.upmc.fr>
2018-04-11 12:40:59 +0100

    few changes in the test file.

--- test/test_fivebody.jl
+++ test/test_fivebody.jl
@@ -3,14 +3,5 @@ using NBodyIPs
 using JuLIP, Base.Test, StaticArrays, BenchmarkTools, Combinatorics
 
 
-Ball = NBodyIPs.nbody_onlytuples(5,5)
-B3 = NBodyIPs.nbody_tuples(4, 10)
-
-
-for len in 4:10
-   Ball = NBodyIPs.nbody_alltuples(4, len)
-   B2 = NBodyIPs.nbody_tuples(2, len)
-   B3 = NBodyIPs.nbody_tuples(3, len)
-   B4 = NBodyIPs.nbody_tuples(4, len)
-   @test length(Ball) == length([B2; B3; B4])
-end
+Ball = NBodyIPs.nbody_onlytuples(4,12)
+B3 = NBodyIPs.nbody_tuples(4, 12)
