f271ec0ee430fe0eded3ac98e3bee7e8e52b9dcb
Genevieve <dusson@ljll.math.upmc.fr>
2018-04-11 12:50:14 +0100

    including 3-body terms in nbody_onlytuples

--- test/test_fivebody.jl
+++ test/test_fivebody.jl
@@ -3,5 +3,7 @@ using NBodyIPs
 using JuLIP, Base.Test, StaticArrays, BenchmarkTools, Combinatorics
 
 
-Ball = NBodyIPs.nbody_onlytuples(4,12)
-B3 = NBodyIPs.nbody_tuples(4, 12)
+Ball = NBodyIPs.nbody_onlytuples(3,12)
+B3 = NBodyIPs.nbody_tuples(3, 12)
+
+trueNbody((1,0,0))
