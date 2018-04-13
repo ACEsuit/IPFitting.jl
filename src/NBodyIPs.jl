5004e2510bb115710e3d7671883071d13545f38b
Christoph Ortner <c.ortner@warwick.ac.uk>
2018-04-13 18:21:59 +0100

    big chunk of drafting invariants

--- src/NBodyIPs.jl
+++ src/NBodyIPs.jl
@@ -1,6 +1,10 @@
 module NBodyIPs
 
-using Reexport
+export bodyorder
+
+
+# prototypes
+function bodyorder
 
 # different standard dictionaries to use
 include("dictionaries.jl")
@@ -8,9 +12,8 @@ include("dictionaries.jl")
 # code for permutation invariant functions (usually polynomials of some sort)
 include("polynomials.jl")
 
-# code to combine the many-body terms from "polynomials.jl" into
-# objects that can evaluate total energies and forces
-include("assembly.jl")
+# describe basis functions in terms of symmetry invariants
+include("invariants.jl")
 
 # fitting from data (e.g., least squares)
 include("fitting.jl")
