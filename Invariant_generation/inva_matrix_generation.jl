0ab37d36e6ea7e9ff3a7649b0448d8bd8939e35c
Genevieve <dusson@ljll.math.upmc.fr>
2018-05-03 13:54:20 +0100

    some working cases for the generation of the invariants.

--- Invariant_generation/inva_matrix_generation.jl
+++ Invariant_generation/inva_matrix_generation.jl
@@ -5,39 +5,39 @@ include("perm_svector_generator.jl")
 
 NBody = 5;
 Deg = 6;
-NBlengths = Int(NBody*(NBody-1)/2);
 
-NB_sec_inv = countlines("Invariant_generation/NBody_$NBody""_deg_$Deg""_invariants.jl");
+# TODO: code this function in a nicer way
+function generate_monomials_irr_sec(NBody,Deg)
+    NBlengths = Int(NBody*(NBody-1)/2);
+    NB_sec_inv = countlines("Invariant_generation/NBody_$NBody""_deg_$Deg""_invariants.jl");
 
-Monomials = Array{Int64, 2}(NB_sec_inv,NBlengths);
-Monomials[:] = 0;
+    Monomials = Array{Int64, 2}(NB_sec_inv,NBlengths);
+    Monomials[:] = 0;
 
-file = open("Invariant_generation/NBody_$NBody""_deg_$Deg""_invariants.jl")
-line = readlines(file)
+    Monomials_simple = Array{Int64, 2}(NB_sec_inv,NBlengths);
+    Monomials_simple[:] = 0;
 
+    file = open("Invariant_generation/NBody_$NBody""_deg_$Deg""_invariants.jl")
+    line = readlines(file)
 
-counter = 0;
-for i=1:length(line)
-    for j=1:NBlengths
-        if contains(line[i], "x[$j]")
-            if contains(line[i], "x[$j]^")
-                for k=1:Deg
-                    if contains(line[i], "x[$j]^$k")
-                        Monomials[i,j] = k
+    for i=1:length(line)
+        for j=1:NBlengths
+            if contains(line[i], "x[$j]")
+                if contains(line[i], "x[$j]^")
+                    for k=1:Deg
+                        if contains(line[i], "x[$j]^$k")
+                            Monomials[i,j] = k
+                            Monomials_simple[i,j] = 1
+                        end
                     end
+                else
+                    Monomials[i,j] = 1
+                    Monomials_simple[i,j] = 1
                 end
-            else
-                Monomials[i,j] = 1
             end
         end
     end
+    return Monomials,Monomials_simple
 end
-display(Monomials)
 
-# open("Invariant_generation/NBody_4_deg_10_invariants.jl") do f
-#     for i in enumerate(eachline(f))
-#       println(i)
-#       str = parse(Float64,i)
-#       display(str)
-#     end
-# end
+generate_monomials_irr_sec(NBody,Deg)
--- Invariant_generation/perm_svector_generator.jl
+++ Invariant_generation/perm_svector_generator.jl
@@ -26,11 +26,14 @@ function indice_2_file(Ind,filename,pref)
    L = size(Ind,1);
    deg = size(Ind,2);
 
-   open(filename, "w") do f
+   Output = [];
+
+   open(filename, "a") do f
       for j=1:deg
          write(f, pref)
          write(f, "_$j = ")
          write(f, "@SVector [")
+         push!(Output, pref*"_$j")
          for i=1:(L-1)
             k = Ind[i,j];
             write(f, "x[$k], ")
@@ -39,17 +42,76 @@ function indice_2_file(Ind,filename,pref)
          write(f, "x[$k] ] \n" )
       end
    end
+   return Output
 end
 
-function monomial_2_file(Perms,Permsref,filename,pref)
-   if maximum(Perms)==1
-      Ind = perm_2_indice(unique(simplex_permutations(Perms)))
-      indice_2_file(Ind,filename,pref)
+# function monomial_2_file(Perms,Permsref,filename,pref)
+filename = "test2.jl";
+pref = "PV1";
+x = @SVector [3,1,0,0,0,0]
+xref = @SVector [1,1,0,0,0,0]
+Permsref = xref
+Perms = x
+
+   # nb of non-zero coefficients
+   perm_deg = sum(Permsref)
+   nb_diff_coef = length(unique(Perms))-1
+   Ind = perm_2_indice(unique(simplex_permutations(Permsref)))
+   Names = indice_2_file(Ind,filename,pref)
+   Decreasing_coef = sort(Perms,rev=true)
+   if (perm_deg==1)
+      open(filename, "a") do f
+         exponent = Decreasing_coef[1];
+         write(f, pref)
+         write(f, " = ")
+         write(f, "sum(x$exponent)\n")
+      end
+   elseif (maximum(Perms)==1)&(perm_deg==2)
+      open(filename, "a") do f
+         write(f, pref)
+         write(f, " = ")
+         write(f, "dot("*Names[1]*","Names[2]")\n")
+      end
+   elseif (maximum(Perms)==1)&(perm_deg==3)
+      open(filename, "a") do f
+         write(f, pref)
+         write(f, " = ")
+         write(f, "dot("*Names[1]*".*"Names[2]","*Names[3]")\n")
+      end
+   elseif (maximum(Perms)==1)&(perm_deg==4)
+      open(filename, "a") do f
+         write(f, pref)
+         write(f, " = ")
+         write(f, "dot("*Names[1]*".*"Names[2]","*Names[3]".*"*Names[4]")\n")
+      end
+   elseif (maximum(Perms)==1)&(perm_deg==5)
+      open(filename, "a") do f
+         write(f, pref)
+         write(f, " = ")
+         write(f, "dot("*Names[1]*".*"Names[2]","*Names[3]".*"*Names[4]".*"*Names[5]")\n")
+      end
+   elseif (Decreasing_coef[1]==2)&(perm_deg==2)
+      open(filename, "a") do f
+         write(f,Names[1],"_2 =",Names[1],".*",Names[1],"\n")
+         write(f,Names[2],"_2 =",Names[2],".*",Names[2],"\n")
+         write(f, pref)
+         write(f, " = ")
+         write(f, "dot("*Names[1]*"_2,"*Names[2]*")+dot("*Names[1]","Names[2]"_2)\n")
+      end
+   elseif (Decreasing_coef[1]==3)&(Decreasing_coef[2]==1)&(perm_deg==2)
+      open(filename, "a") do f
+         write(f,Names[1],"_2 =",Names[1],".*",Names[1],"\n")
+         write(f,Names[2],"_2 =",Names[2],".*",Names[2],"\n")
+         write(f,Names[1],"_3 =",Names[1],"_2.*",Names[1],"\n")
+         write(f,Names[2],"_3 =",Names[2],"_2.*",Names[2],"\n")
+         write(f, pref)
+         write(f, " = ")
+         write(f, "dot("*Names[1]*"_3,"*Names[2]*")+dot("*Names[1]","Names[2]"_3)\n")
+      end
    else
-      Ind = perm_2_indice(unique(simplex_permutations(Permsref)))
-      indice_2_file(Ind,filename,pref)
+      error("not implemented yet")
    end
-end
+# end
 
 x = @SVector [2,1,0,0,0,0]
 xref = @SVector [1,1,0,0,0,0]
