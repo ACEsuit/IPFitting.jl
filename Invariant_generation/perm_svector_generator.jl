a23a972122f41737446590822ed26106cf924e23
Genevieve <dusson@ljll.math.upmc.fr>
2018-05-04 07:40:16 +0100

    clean up +tests of irreducible secondaries : working !

--- Invariant_generation/perm_svector_generator.jl
+++ Invariant_generation/perm_svector_generator.jl
@@ -1,8 +1,9 @@
 using Combinatorics, StaticArrays, NBodyIPs
-# include("misc.jl")
+include("misc.jl")
 
 
-function generate_monomials(filename,NBvariables)
+function generate_monomials(filename,NBlengths,Deg=10)
+    # Deg is the max. degree
     NB_sec_inv = countlines(filename);
 
     Monomials = [];
@@ -35,11 +36,13 @@ function generate_monomials(filename,NBvariables)
     return NB_sec_inv,Monomials,Monomials_simple
 end
 
+
+
 function generate_monomials_irr_sec(NBody,Deg)
     NBlengths = Int(NBody*(NBody-1)/2);
-
     return generate_monomials("Invariant_generation/NBody_$NBody""_deg_$Deg""_invariants.jl",NBlengths)
 end
+
 
 function printmatrix(filename,A)
     M = size(A,1);
@@ -63,8 +66,7 @@ function printmatrix(filename,A)
     end
 end
 
-function _generate_matrix_Deg_2(NBody,Perms)
-    NBlengths = Int(NBody*(NBody-1)/2);
+function _generate_matrix_Deg_2(NBlengths,Perms)
     Ind = perm_2_indice(unique(simplex_permutations(Perms)))
     A = zeros(Int64, NBlengths,NBlengths);
     for i=1:size(Ind, 1)
@@ -73,87 +75,110 @@ function _generate_matrix_Deg_2(NBody,Perms)
     return A
 end
 
-
-# function generate_all_irr_sec(NBody,Deg) =
-#     _generate_all_irr_sec(filename)
-# end
-
-
 function generate_all_irr_sec(NBody,Deg)
-   (NB_sec_inv,Monomials,Monomials_simple) = generate_monomials_irr_sec(NBody,Deg)
-   filename = "Invariant_generation/NBody_$NBody"*"irr_sec_deg_$Deg.jl";
-   open(filename, "w") do f
-      write(f, "# Irreducible secondaries for NBody=$NBody"*"and deg=$Deg \n")
-   end
-   for j=1:NB_sec_inv
-      pref = "PV$j";
-      Perms = SVector(Monomials[j]...)
-      Permsref = SVector(Monomials_simple[j]...)
-      perm_deg = sum(Permsref)
-      nb_diff_coef = length(unique(Perms))-1
-      Perm_inv = sort(Perms,rev=true)
-      # TODO: keep track of matrices
+    NBlengths = Int(NBody*(NBody-1)/2);
+    filename = "Invariant_generation/NBody_$NBody"*"irr_sec_deg_$Deg.jl";
+    filenamedata = "Invariant_generation/NBody_$NBody""_deg_$Deg""_invariants.jl"
+    preword = "# Irreducible secondaries for NBody=$NBody"*"and deg=$Deg \n"
+    return generate_invariants(filenamedata,filename,NBlengths,Deg,preword)
 
-      # different case with respect to the number of variables in the monomial
-      if (perm_deg==1)
-          # only 1 variable -> sum of x to some power
-          open(filename, "a") do f
-             exponent = Perm_inv[1];
-             write(f, pref)
-             write(f, " = ")
-             write(f, "sum(x$exponent)\n")
-          end
-
-      elseif (perm_deg==2)
-          A = _generate_matrix_Deg_2(NBody,Perms)
-          exp1 = Perm_inv[1];
-          exp2 = Perm_inv[2];
-          open(filename, "a") do f
-             write(f, "A$j=")
-         end
-             printmatrix(filename,A)
-         open(filename, "a") do f
-             write(f,"\n")
-             write(f, pref)
-             write(f, " = ")
-             write(f, "x$exp1","'*","A$j","*"," x$exp2")
-             write(f, "\n\n")
-          end
-
-      else # (perm_deg>2)
-          for s=1:perm_deg
-              exp = Perm_inv[s]
-              Ind = perm_2_indice(unique(simplex_permutations(Perms)))
-              open(filename, "a") do f
-                  # First vector
-                  write(f, pref, "_$s = ")
-                  write(f, "@SVector [")
-                  for j = 1:size(Ind,1)
-                      k = Ind[j,s]
-                      write(f, "x$exp[$k],")
-                  end
-                  write(f, "] \n")
-              end
-          end
-          if (perm_deg == 3)
-              open(filename, "a") do f
-                  write(f, pref, " = ")
-                  write(f, "dot(", pref, "_1.*", pref, "_2,", pref, "_3)\n \n")
-              end
-          elseif (perm_deg == 4)
-              open(filename, "a") do f
-                  write(f, pref, " = ")
-                  write(f, "dot(", pref, "_1.*", pref, "_2,", pref, "_3.*", pref, "_4)\n \n")
-              end
-          elseif     (perm_deg == 5)
-              open(filename, "a") do f
-                  write(f, pref, " = ")
-                  write(f, "dot(", pref, "_1.*", pref, "_2.*", pref, "_3,", pref, "_4.*", pref, "_5)\n \n")
-              end
-          else
-              error("not implemented (perm_deg larger than 5)")
-          end
-      end
-   end
-   return 0
 end
+
+function generate_invariants(filenamedata,filename,NBlengths,Deg,preword)
+    (NB_sec_inv,Monomials,Monomials_simple) = generate_monomials(filenamedata,NBlengths,Deg)
+    open(filename, "w") do f
+       write(f, preword )
+    end
+    for j=1:NB_sec_inv
+       pref = "PV$j";
+       Perms = SVector(Monomials[j]...)
+       Permsref = SVector(Monomials_simple[j]...)
+       perm_deg = sum(Permsref)
+       nb_diff_coef = length(unique(Perms))-1
+       Perm_inv = sort(Perms,rev=true)
+       # TODO: keep track of matrices
+
+       # different case with respect to the number of variables in the monomial
+       if (perm_deg==1)
+           # only 1 variable -> sum of x to some power
+           open(filename, "a") do f
+              exponent = Perm_inv[1];
+              write(f, pref)
+              write(f, " = ")
+              write(f, "sum(x$exponent)\n")
+           end
+
+       elseif (perm_deg==2)
+           A = _generate_matrix_Deg_2(NBlengths,Perms)
+           exp1 = Perm_inv[1];
+           exp2 = Perm_inv[2];
+           open(filename, "a") do f
+              write(f, "A$j=")
+          end
+              printmatrix(filename,A)
+          open(filename, "a") do f
+              write(f,"\n")
+              write(f, pref)
+              write(f, " = ")
+              write(f, "x$exp1","'*","A$j","*"," x$exp2")
+              write(f, "\n\n")
+           end
+
+       else # (perm_deg>2)
+           for s=1:perm_deg
+               exp = Perm_inv[s]
+               Ind = perm_2_indice(unique(simplex_permutations(Perms)))
+               open(filename, "a") do f
+                   # First vector
+                   write(f, pref, "_$s = ")
+                   write(f, "@SVector [")
+                   for j = 1:size(Ind,1)
+                       k = Ind[j,s]
+                       write(f, "x$exp[$k],")
+                   end
+                   write(f, "] \n")
+               end
+           end
+           if (perm_deg == 3)
+               open(filename, "a") do f
+                   write(f, pref, " = ")
+                   write(f, "dot(", pref, "_1.*", pref, "_2,", pref, "_3)\n \n")
+               end
+           elseif (perm_deg == 4)
+               open(filename, "a") do f
+                   write(f, pref, " = ")
+                   write(f, "dot(", pref, "_1.*", pref, "_2,", pref, "_3.*", pref, "_4)\n \n")
+               end
+           elseif     (perm_deg == 5)
+               open(filename, "a") do f
+                   write(f, pref, " = ")
+                   write(f, "dot(", pref, "_1.*", pref, "_2.*", pref, "_3,", pref, "_4.*", pref, "_5)\n \n")
+               end
+           else
+               error("not implemented (perm_deg larger than 5)")
+           end
+       end
+    end
+    return 0
+
+end
+
+
+function perm_2_indice(Perms)
+   L = length(Perms);
+   deg = length(find(Perms[1]));
+   M = length(Perms[1]);
+
+   Indices = Array{Int64, 2}(L,deg);
+   # zeros(L,deg);
+
+      for j=1:L
+         Ind_temp = sortperm(Perms[j],rev=true)
+         Indices[j,:] = Ind_temp[1:deg]
+      end
+   return Indices
+end
+
+
+
+generate_all_irr_sec(5,6)
