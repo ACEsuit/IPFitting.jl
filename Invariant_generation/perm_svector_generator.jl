b279025575b002c1e84220ad91b0010ea433e120
Genevieve <dusson@ljll.math.upmc.fr>
2018-05-04 06:11:54 +0100

    temp

--- Invariant_generation/perm_svector_generator.jl
+++ Invariant_generation/perm_svector_generator.jl
@@ -76,31 +76,6 @@ end
 A= _generate_matrix_Deg_2(4,SVector(2,1,0,0,0,0))
 
 
-function _generate_matrix_Deg_2_diff(NBody,Deg,Perms)
-    NBlengths = Int(NBody*(NBody-1)/2);
-    Ind = perm_2_indice(unique(simplex_permutations(Perms)))
-    A = zeros(Int64, NBlengths,NBlengths);
-    for i=1:size(Ind, 1)
-        A[Ind[i,1],Ind[i,2]] = 1;
-    end
-    return Ind, A
-end
-
-_generate_matrix_Deg_2_diff(4,10,SVector(1, 1, 0, 0, 0, 0))
-
-function _generate_matrix_Deg_2_same(NBody,Deg,Perms)
-    Ind = perm_2_indice(unique(simplex_permutations(Perms)))
-    A = zeros(Int64, NBlengths,NBlengths);
-    for i=1:size(Ind, 1)
-        A[Ind[i,1],Ind[i,2]] = 1;
-    end
-    return A
-end
-
-_generate_matrix_Deg_2_same(4,10,SVector(2, 1, 0, 0, 0, 0))
-
-2;
-
 
 function generate_all_irr_sec(NBody,Deg)
    (NB_sec_inv,Monomials,Monomials_simple) = generate_monomials_irr_sec(NBody,Deg)
@@ -140,15 +115,43 @@ function generate_all_irr_sec(NBody,Deg)
              write(f, pref)
              write(f, " = ")
              write(f, "x$exp1","'*","A$j","*"," x$exp2")
-             write(f, "\n")
+             write(f, "\n\n")
           end
 
       elseif (perm_deg==3)
+          for s=1:perm_deg
+          exp = Perm_inv[s]
+          Ind = perm_2_indice(unique(simplex_permutations(Perms)))
           exp1 = Perm_inv[1];
           exp2 = Perm_inv[2];
           exp3 = Perm_inv[3];
           open(filename, "a") do f
-             write(f, "A$j=")
+              # First vector
+              write(f, pref, "_1 = ")
+              write(f, "@SVector [")
+              for j = 1:size(Ind,1)-1
+                  k = Ind[j,1]
+                  write(f, "x$exp1[$k],")
+              end
+              write(f, "] \n")
+              # Second vector
+              write(f, pref, "_2 = ")
+              write(f, "@SVector [")
+              for j = 1:size(Ind,1)-1
+                  k = Ind[j,2]
+                  write(f, "x$exp2[$k],")
+              end
+              write(f, "] \n")
+              # Third vector
+              write(f, pref, "_3 = ")
+              write(f, "@SVector [")
+              for j = 1:size(Ind,1)-1
+                  k = Ind[j,3]
+                  write(f, "x$exp3[$k],")
+              end
+              write(f, "] \n")
+              write(f, pref, " = ")
+              write(f, "dot(", pref, "_1.*", pref, "_2,", pref, "_3)\n \n")
          end
 
       elseif (perm_deg==4)
