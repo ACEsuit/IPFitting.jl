a1a2a45924f5f09db32c484d4ec6699da2bb5ff5
Christoph Ortner <c.ortner@warwick.ac.uk>
2018-05-30 21:30:58 +0100

    towards the temporary replacement iterator

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using BenchmarkTools
 @testset "NBodyIPs" begin
    # TODO: monomials testset
    # TODO: fast_polys testset
+   @testset "Misc" begin include("test_misc.jl") end 
    @testset "Invariants" begin include("test_invariants.jl") end
    @testset "Polynomials" begin include("test_polynomials.jl") end
    # TODO: fitting testset
