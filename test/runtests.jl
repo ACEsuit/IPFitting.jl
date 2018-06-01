6f56edc5bbe1bad9d3817a092c1f1b9ff6c5a5e8
Christoph Ortner <c.ortner@warwick.ac.uk>
2018-06-01 20:42:56 +0100

    final bug was in JuLIP - this commit just cleans up the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,10 +5,10 @@ using BenchmarkTools
 @testset "NBodyIPs" begin
    # TODO: monomials testset
    # TODO: fast_polys testset
-   # @testset "Misc" begin include("test_misc.jl") end
+   @testset "Misc" begin include("test_misc.jl") end
    @testset "Iterators" begin include("test_iterators.jl") end
    @testset "Invariants" begin include("test_invariants.jl") end
-   # @testset "Polynomials" begin include("test_polynomials.jl") end
+   @testset "Polynomials" begin include("test_polynomials.jl") end
    # TODO: fitting testset
    # TODO: Data testset
    # TODO: IO / serialization testset
