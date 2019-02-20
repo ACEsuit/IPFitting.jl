24f088835e2223567e0f8d840e13c28b071fec8d
dussong <g.dusson@warwick.ac.uk>
2019-02-20 17:32:36 +0000

    lsqerrs for res_dict.

--- src/lsqerrors.jl
+++ src/lsqerrors.jl
@@ -289,13 +289,13 @@ function results_dict(data, IP; confignames = Colon(), pathname = "")
    confignames = allconfignames
 
    # create the dict for the results
-   results = Dict{String, Dict{String,Vector{Tuple{Vector{Float64},Vector{Float64}}}}}()
+   results = Dict{String, Dict{String,Vector{Tuple{Vector{Float64},Vector{Float64},Int64}}}}()
    for cn in confignames
-      results[cn] = Dict{String,Vector{Tuple{Vector{Float64},Vector{Float64}}}}()
+      results[cn] = Dict{String,Vector{Tuple{Vector{Float64},Vector{Float64},Int64}}}()
    end
 
-   for dat in data
-      #  print(".")
+   for (i,dat) in enumerate(data)
+       # print(".")
       ct = configtype.(dat)
       cn = configname.(dat)
       if !(cn in confignames)
@@ -307,10 +307,10 @@ function results_dict(data, IP; confignames = Colon(), pathname = "")
             continue
          end
          if !haskey(results[cn], ot)
-            results[cn][ot] = Vector{Tuple{Vector{Float64},Vector{Float64}}}[]
+            results[cn][ot] = Vector{Tuple{Vector{Float64},Vector{Float64},Int64}}[]
          end
          # Store exact data + approximation
-            push!(results[cn][ot], (observation(dat,ot) , (ipcomp(ASEAtoms(dat.at),ot,IPf))) )
+            push!(results[cn][ot], (observation(dat,ot) , (ipcomp(ASEAtoms(dat.at),ot,IPf)),i) )
       end
    end
    if pathname != ""
@@ -319,24 +319,113 @@ function results_dict(data, IP; confignames = Colon(), pathname = "")
    return results
 end
 
-function cdf_energy_forces(res_dict; nb_points_cdf = 40)
-   absEerr = Float64[]
-   absFerr = Float64[]
-   absVerr = Float64[]
-   for cn in collect(keys(res_dict))
-      if haskey(res_dict[cn],"E")
-         for i in 1:length(res_dict[cn]["E"])
-            append!(absEerr, abs.(res_dict[cn]["E"][i][1] - res_dict[cn]["E"][i][2]) )
-         end
+
+function lsqerrors(res_dict, data; confignames = Colon(), nb_points_cdf = 40)
+   if confignames isa Colon
+      confignames = collect(keys(res_dict))
+   else
+      confignames = collect(confignames)
+   end
+
+   # create the dict for the fit errors
+   errs = LsqErrors([confignames; "set"])
+   lengths = Dict{String, Dict{String, Int}}()
+   for cn in [confignames; "set"]
+      lengths[cn] = Dict{String, Int}()
+   end
+
+   for cn in keys(res_dict)
+      if !(cn in confignames)
+         continue
       end
-      if haskey(res_dict[cn],"F")
-         for i in 1:length(res_dict[cn]["F"])
-            append!(absFerr, abs.(res_dict[cn]["F"][i][1] - res_dict[cn]["F"][i][2]) )
+      # loop through observation types in the current config type
+      for ot in ["E", "F", "V"]
+         if !haskey(res_dict[cn], ot)
+            continue
          end
+         if !haskey(errs.rmse[cn], ot)
+            errs.rmse[cn][ot] = 0.0
+            errs.nrm2[cn][ot] = 0.0
+            errs.mae[cn][ot] = 0.0
+            errs.nrm1[cn][ot] = 0.0
+            errs.allerr[cn][ot] = Float64[]
+            lengths[cn][ot] = 0
+         end
+         if !haskey(errs.rmse["set"], ot)
+            errs.rmse["set"][ot] = 0.0
+            errs.nrm2["set"][ot] = 0.0
+            errs.mae["set"][ot] = 0.0
+            errs.nrm1["set"][ot] = 0.0
+            errs.allerr["set"][ot] = Float64[]
+            lengths["set"][ot] = 0
+         end
+         # loop over configurations
+         for d in res_dict[cn][ot]
+            i = d[3]
+            y = d[2]
+            yapprox = d[1]
+            # get the approximate computation
+            # compute the various errors for this data/ot combination
+            w = weighthook(ot, data[i])
+            errs.rmse[cn][ot] += w^2 * norm(yapprox - y)^2
+            errs.nrm2[cn][ot] += w^2 * norm(y)^2
+            errs.mae[cn][ot]  += w * norm(yapprox - y, 1)
+            errs.nrm1[cn][ot] += w * norm(y, 1)
+            append!(errs.allerr[cn][ot], w * abs.(yapprox - y) )
+            lengths[cn][ot] += length(y)
+            errs.rmse["set"][ot] += w^2 * norm(yapprox - y)^2
+            errs.nrm2["set"][ot] += w^2 * norm(y)^2
+            errs.mae["set"][ot]  += w * norm(yapprox - y, 1)
+            errs.nrm1["set"][ot] += w * norm(y, 1)
+            lengths["set"][ot] += length(y)
+            append!(errs.allerr["set"][ot], w * abs.(yapprox - y) )
+            # @show length(errs.allerr["set"][ot])
+            # @show lengths["set"][ot]
+         end
+
       end
    end
-   return quantile(absEerr, collect(linspace(0.,1.,nb_points_cdf))), quantile(absFerr, collect(linspace(0.,1.,nb_points_cdf)))
+
+   for cn in [confignames; "set"]
+      for ot in keys(errs.rmse[cn])
+         len = lengths[cn][ot]
+         errs.rmse[cn][ot] = sqrt( errs.rmse[cn][ot] / len )
+         errs.nrm2[cn][ot] = sqrt( errs.nrm2[cn][ot] / len )
+         errs.mae[cn][ot] = errs.mae[cn][ot] / len
+         errs.nrm1[cn][ot] = errs.nrm1[cn][ot] / len
+         errs.allerr[cn][ot] = quantile(errs.allerr[cn][ot] ,linspace(0.,1.,nb_points_cdf))
+      end
+   end
+
+   # # ------- - scatter E -------
+   # push!(scatterE[ct][1], E_data/len)
+   # push!(scatterE[ct][2], E_fit/len)
+   # append!(scatterF[ct][1], f_data)
+   # append!(scatterF[ct][2], f_fit)
+
+   return errs
+
+
 end
+
+# function cdf_energy_forces(res_dict; nb_points_cdf = 40)
+#    absEerr = Float64[]
+#    absFerr = Float64[]
+#    absVerr = Float64[]
+#    for cn in collect(keys(res_dict))
+#       if haskey(res_dict[cn],"E")
+#          for i in 1:length(res_dict[cn]["E"])
+#             append!(absEerr, abs.(res_dict[cn]["E"][i][1] - res_dict[cn]["E"][i][2]) )
+#          end
+#       end
+#       if haskey(res_dict[cn],"F")
+#          for i in 1:length(res_dict[cn]["F"])
+#             append!(absFerr, abs.(res_dict[cn]["F"][i][1] - res_dict[cn]["F"][i][2]) )
+#          end
+#       end
+#    end
+#    return quantile(absEerr, collect(linspace(0.,1.,nb_points_cdf))), quantile(absFerr, collect(linspace(0.,1.,nb_points_cdf)))
+# end
 
 
 
