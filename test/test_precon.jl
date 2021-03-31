c8895f35961545466bb895f5c00e17d127da6b19
Christoph Ortner <c.ortner@warwick.ac.uk>
2021-03-31 11:04:55 -0700

    account for infinitesiam rotations, innerstab parameter

--- test/test_precon.jl
+++ test/test_precon.jl
@@ -4,41 +4,47 @@ module Preconditioners
 
 using JuLIP, LinearAlgebra
 
-# @doc raw"""
-# `AdjacancyPrecon`
-# ```math
-#    P_{ij} = k(n) \qquad \text{if} \quad
-#    r_0(n) \leq r_{ij} \leq r_1(n)
-# ```
-# """
+@doc raw"""
+`AdjacancyPrecon` :
+
+If `(r0, r1, k)` is in the `intervals` list and if
+`r0 <= rij <= r1` then the 3 x 3 block
+```math
+   P_{ij} = k \big( (1-c) \hat{R} \otimes \hat{R} + c I \big),
+```
+where ``\hat{R}`` is the bond direction and `c = innerstab`.
+* Choose `innerstab = 0` to get full infinitesimal rotation-invariance
+* Choose `innerstab = 1` to get full resistance against infinitesimal rotations
+* In optimisation we often found that `innerstab = 0.1` is a decent choice.
+
+In addition the `stab` parameter adds a global stabilisation shifting
+the entire matrix by `stab * I`.
+"""
 struct AdjacancyPrecon
    intervals::Vector{NamedTuple{(:r0, :r1, :k), Tuple{Float64, Float64, Float64}}}
    stab::Float64
-   infrot::Bool
+   innerstab::Float64
 end
 
 import JuLIP.FIO: read_dict, write_dict
 
 
-
-AdjacancyPrecon(intervals; stab = 1e-4, infrot=false) =
-   AdjacancyPrecon(intervals, stab, infrot)
+AdjacancyPrecon(intervals; stab = 1e-4, innerstab = 0.1) =
+   AdjacancyPrecon(intervals, stab, innerstab)
 
 _cutoff(P::AdjacancyPrecon) =
    maximum( i.r1 for i in P.intervals )
 
 function (P::AdjacancyPrecon)(R::JVecF)
    r = norm(R)
-   if !P.infrot
-      for i in P.intervals
-         if i.r0 <= r <= i.r1
-            return i.k * Matrix(I, (3,3))
-         end
+   R̂ = R / r
+   Π = (1 - P.innerstab) * R̂ * R̂' + P.innerstab * one(JMat{Float64})
+   for i in P.intervals
+      if i.r0 <= r <= i.r1
+         return i.k * Π
       end
-   else
-      error("infrot not implemented yet")
    end
-   return 0.0
+   return 0.0 * Π
 end
 
 function (precon::AdjacancyPrecon)(at::Atoms)
@@ -66,38 +72,38 @@ end
 end
 
 #---
-using JuLIP
 
 #---
+# CAS - please convert this into a test set:
+
+@info("Test evaluation and some basic properties of AdjacancyPrecon")
+
+using JuLIP, Test
 
 at = bulk(:W, cubic=true) * 2
 
-adjp = Preconditioners.AdjacancyPrecon( [ (r0 = 2.0, r1 = 3.5, k = 1.0),
-                                          (r0 = 3.5, r1 = 4.5, k = 0.1) ] )
-P = adjp(at)
+intervals = [ (r0 = 2.0, r1 = 3.5, k = 1.0),
+              (r0 = 3.5, r1 = 4.5, k = 0.1) ]
 
-P' ≈ P
-eigvals(P)
+# the block-Identity preconditoner
+adjp1 =  Preconditioners.AdjacancyPrecon(intervals; innerstab = 1.0)
+# a variation that accounts for some infinitesimal rotational invariance
+# the parameter innerstab can interpolate between the two.
+adjp2 =  Preconditioners.AdjacancyPrecon(intervals; innerstab = 0.1)  # <= DEFAULT
+adjp3 =  Preconditioners.AdjacancyPrecon(intervals; innerstab = 0.0)
 
-weights = Dict()
-weights["precon"] = Dict("default" => adjp)
+for adjp in [adjp1, adjp2, adjp3]
+   P = adjp(at)
+   println(@test P' ≈ P)
+   sumP = sum(P, dims=1)
+   println(@test all(sumP .≈ adjp.stab))
+   sumP = sum(P, dims=2)
+   println(@test norm(sumP .- adjp.stab, Inf) < 1e-12)
+   spec = sort(eigvals(P))
+   println(@test all(spec[1:3] .≈ adjp.stab))
+end
 
 #---
-
-
-using LinearAlgebra
-A = rand(10,10)
-P = A' * A
-
-rtP1 = sqrt(P)
-rtP1 * rtP1 ≈ P
-
-rtP2 = cholesky(P, Val(false)).L
-pinv(rtP2') * pinv(rtP2) ≈ pinv(P)
-
-
-B = rand(10, 100)
-pinv(rtP2) * B ≈ rtP2 \ B
-
-
-Matrix(I, (3,3))
+# # how to use:
+# weights = Dict()
+# weights["precon"] = Dict("default" => adjp)
